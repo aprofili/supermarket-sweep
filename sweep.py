526cf04118f78181a60498b7aba62c50b2720b05
aprofili <aprofili@gmail.com>
2021-04-14 22:32:07 -0400

    working!! i think

--- sweep.py
+++ sweep.py
@@ -50,32 +50,36 @@ from gurobipy import GRB, Model,quicksum
 
 m = Model('Supermarket Sweep')
 
-n= 5#len(item_list)
+n= len(item_list)
+print(n)
 v=[i.price for i in item_list]
 v.append(0.0)
 maxtime=90
 #print(v)
 #print(len(d[0]),len(d),len(v))
-d=[[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6]]
+# d=[[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6]]
 
 x=m.addVars(range(1,n+1), range(2,n+2), vtype=GRB.BINARY, name='x') #if path from item i to j is in the sweep
-y=m.addVars(range(1,n+2), vtype=GRB.CONTINUOUS, name='y',lb=0) #seconds from start to item 1 during sweep
-t=m.addVars(range(1,n+1), range(2,n+2), vtype=GRB.CONTINUOUS, name='t',lb=0) 
+y=m.addVars(range(1,n+2), vtype=GRB.CONTINUOUS, name='y',lb=0) #seconds from start to item i during sweep
+t=m.addVars(range(1,n+1), range(2,n+2), vtype=GRB.CONTINUOUS, name='t',lb=0) # seconds from start to item j if node j comes after node i
 
-m.addConstr(y[1]==0)
-m.addConstrs(x[i,i]==0 for i in range(2,n+1))
-m.addConstrs(quicksum([x[i,j] for i in range(1,n+1)])==1 for j in range(2,n+2))
-m.addConstrs(quicksum([x[i,j] for j in range(2,n+2)])==1 for i in range(1,n+1))
-#m.addConstrs(0<=t[i,j] for i in range(1,n+1) for j in range(2,n+2))
-m.addConstrs(t[i,j]<=maxtime*x[i,j] for i in range(1,n+1) for j in range(2,n+2))
-m.addConstrs(y[j]==quicksum([t[i,j] for i in range(1,n+1)]) for j in range(2,n+2))
-m.addConstrs(quicksum([t[j,k] for k in range(2,n+2)])==y[j]+quicksum([d[j-1][k-1]*x[j,k] for k in range(2,n+2) ]) for j in range(1,n+1))
-#m.addConstr(y[n+1]<=90)
-#m.addConstr(quicksum([t[i,n+1] for i in range(1,n+1)])==1)
+m.addConstr(y[1]==0) # starts with node 1  and time 0
+m.addConstrs(x[i,i]==0 for i in range(2,n+1)) # nodes cannot connect to themselves
+m.addConstrs(quicksum([x[i,j] for i in range(1,n+1)])<=1 for j in range(2,n+2)) # all nodes follow 0 or 1 node
+m.addConstrs(quicksum([x[i,j] for j in range(2,n+2)])<=1 for i in range(1,n+1)) # all nodes are followed by 0 or 1 node
+m.addConstrs(t[i,j]<=maxtime*x[i,j] for i in range(1,n+1) for j in range(2,n+2)) # if node i doesn't go to node j, then t[i, j] is 0
+m.addConstrs(y[j]==quicksum([t[i,j] for i in range(1,n+1)]) for j in range(2,n+2)) # y[j] is equal to the only positive t[i, j]
+m.addConstrs(quicksum([t[j,k] for k in range(2,n+2)])==y[j]+quicksum([d[j-1][k-1]*x[j,k] for k in range(2,n+2) ]) for j in range(1,n+1)) # defines t[i, j] to be the time up to the i plus the time from the i to j
+m.addConstr(y[n+1]<=90) # total time less than 90 seconds
+m.addConstr(quicksum([x[i, n+1] for i in range(1, n+1)]) == 1) # node n+1 must be visited
+
+# m.addConstr(quicksum([x[n+1, j] for i in range(1, n+1)]) == 0) # node n+1 must come last
+# m.addConstrs(y[n+1] >= y[i] for i in range(1, n+1)) #node n+1 must come last
 
 
-m.setObjective(y[n+1],GRB.MINIMIZE)
-#m.setObjective(quicksum([v[i]*quicksum([x[i,j] for j in range(i+1,n+2)]) for i in range(1,n+1)]), GRB.MAXIMIZE)
+# m.setObjective(y[n+1],GRB.MINIMIZE) # minimize time to the last node
+# m.setObjective(quicksum([quicksum([d[i-1][j-1] * x[i, j] for j in range(2, n+2)]) for i in range(1, n+1)]),GRB.MINIMIZE) # same as previous
+m.setObjective(quicksum([v[i]*quicksum([x[i,j] for j in range(i+1,n+2)]) for i in range(1,n+1)]), GRB.MAXIMIZE) # maximize cost of items collected
 
 m.optimize()
 #class Node:
@@ -85,14 +89,26 @@ m.optimize()
 #    def __repr__(self):
 #        return 'item '+ str(self.i) + " comes before item " + str(self.next.item)
 
-nodedict={}
-for i in range(1,n+1):
-    j=i+1
-    while j <n+2:
-        if int(x[i,j].x):
-            nodedict[i]=j
-            j=n+2
-        j+=1
-print(nodedict)
-
 print("\nOptimal Value: %s" % (str(m.ObjVal)))
+print("\n\n")
+
+nodedict={}
+count = 0
+for i in range(1,n+1):
+    for j in range(2,n+2):
+        if int(x[i,j].x):
+            count +=1
+            nodedict[i]=j
+
+i = 1
+print(i)
+for rep in range(count):
+    i = nodedict[i]
+    print(i)
+
+# print(count)
+time = 0
+for (i, j) in nodedict.items():
+    time += d[i-1][j-1]
+# print(time)
+

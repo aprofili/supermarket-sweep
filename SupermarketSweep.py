0faed5b37e968e2b155ff313d69fa72c65d1e85c
Brynn Schneberger <brynn.schne@gmail.com>
2021-04-24 20:16:22 -0400

    Update SupermarketSweep.py

--- SupermarketSweep.py
+++ SupermarketSweep.py
@@ -2,6 +2,7 @@
 import csv
 from pprint import pprint
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Item:
     def __init__(self, name, price, x, y):
@@ -36,10 +37,10 @@ for i in range(len(item_list)):
             dist_y = min(((110 - item_i.y) + (110 - item_j.y) ), item_i.y + item_j.y)
             d[i][j] = ((dist_x + dist_y) / 10) 
             d[j][i] = ((dist_x + dist_y) / 10) 
-        # if i!=j:
-        #     d[j][i]+=2
-        #     d[i][j] +=2
-#
+        if i!=j:
+            d[j][i]+=2
+            d[i][j] +=2
+
 #d = [[0 for i in range(len(item_list))] for i in range(len(item_list))]
 #for i in range(len(item_list)):
 #    for j in range(len(item_list)):
@@ -53,9 +54,9 @@ for i in range(len(item_list)):
 #            dist_y = min(((110 - item_i.y) + (110 - item_j.y) ), item_i.y + item_j.y)
 #            d[i][j] = (dist_x + dist_y) / 10
 
-#.append(d[0])
+d.append(d[0])
 for i in range(len(d[0])):
-    d[i].append(max(d[i][0],0))
+    d[i].append(max(d[i][0]-2,0))
 pprint(np.array(d))
 
 #for i in range(len(d)):
@@ -92,12 +93,12 @@ def optimize(max_time=90, cart_cap=15, mip_gap=0.0001, print_output=False):
     m.addConstr(quicksum([x[i, n+1] for i in range(1, n+1)]) == 1) # node n+1 must be visited
     m.addConstrs(t[i,j]<=max_time*x[i,j] for i in range(1,n+1) for j in range(2,n+2)) # if node i doesn't go to node j, then t[i, j] is 0
     m.addConstrs(y[j]==quicksum([t[i,j] for i in range(1,n+1)]) for j in range(2,n+2)) # y[j] is equal to the only positive t[i, j]
-    m.addConstrs(quicksum([t[j,k] for k in range(2,n+2)])==y[j]+quicksum([(d[j-1][k-1]+2)*x[j,k] for k in range(2,n+2) ]) for j in range(1,n+1)) # defines t[i, j] to be the time up to the ith node plus the time from the ith to jth node
+    m.addConstrs(quicksum([t[j,k] for k in range(2,n+2)])==y[j]+quicksum([(d[j-1][k-1])*x[j,k] for k in range(2,n+2) ]) for j in range(1,n+1)) # defines t[i, j] to be the time up to the ith node plus the time from the ith to jth node
     m.addConstr(y[n+1]<=max_time) # total time less than 90 seconds
     m.addConstr(quicksum([quicksum([x[i,j] for i in range(1,n+1)]) for j in range(2,n+2)])<= cart_cap + 1) #at most 15 items in the cart (not including the end node)
     # m.addConstr(quicksum([x[n+1, j] for i in range(1, n+1)]) == 0) # node n+1 must come last
     # m.addConstrs(y[n+1] >= y[i] for i in range(1, n+1)) #node n+1 must come last
-
+    m.addConstrs(quicksum([x[i,j] for j in range(2,n+2)])== quicksum([x[k,i] for k in range(1,n+1)]) for i in range(2,n+1))
 
     # m.setObjective(y[n+1],GRB.MINIMIZE) # minimize time to the last node
     # m.setObjective(quicksum([quicksum([d[i-1][j-1] * x[i, j] for j in range(2, n+2)]) for i in range(1, n+1)]),GRB.MINIMIZE) # same as previous
@@ -145,7 +146,7 @@ def optimize(max_time=90, cart_cap=15, mip_gap=0.0001, print_output=False):
 
 
 
-parts = ""
+parts = "c"
 max_times = range(80, 101, 5)
 cart_caps = range(5, 26)
 mip_gaps = range(5, 16)

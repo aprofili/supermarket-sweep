eb80fb1986fa22cd5571f407133c1cb091bd59d5
Brynn Schneberger <brynn.schne@gmail.com>
2021-04-25 15:35:56 -0400

    code without comments and part a shorter

--- SupermarketSweep.py
+++ SupermarketSweep.py
@@ -1,8 +1,6 @@
-#import numpy as np
+#Adam Profili and Brynn Schneberger Supermarket Sweeper ISyE 4133 Project
 import csv
-from pprint import pprint
 import matplotlib.pyplot as plt
-import numpy as np
 
 class Item:
     def __init__(self, name, price, x, y):
@@ -21,8 +19,6 @@ with open("Supermarket Sweep.csv", "r") as file:
     for row in reader:
         item_list.append(Item(row[0], row[1], row[2], row[3]))
 
-#pprint(item_list)
-
 d = [[0 for i in range(len(item_list))] for i in range(len(item_list))]
 for i in range(len(item_list)):
     for j in range(i,len(item_list)):
@@ -37,26 +33,9 @@ for i in range(len(item_list)):
             dist_y = min(((110 - item_i.y) + (110 - item_j.y) ), item_i.y + item_j.y)
             d[i][j] = ((dist_x + dist_y) / 10)
             d[j][i] = ((dist_x + dist_y) / 10)
-        if i!=j:            #brynn
-            d[j][i]+=2      #brynn
-            d[i][j]+=2     #brynn
-
-#d = [[0 for i in range(len(item_list))] for i in range(len(item_list))]
-#for i in range(len(item_list)):
-#    for j in range(len(item_list)):
-#        item_i = item_list[i]
-#        item_j = item_list[j]
-#
-#        if item_i.x == item_j.x:
-#            d[i][j] = abs(item_i.y - item_j.y) / 10
-#        else:
-#            dist_x = abs(item_i.x - item_j.x)
-#            dist_y = min(((110 - item_i.y) + (110 - item_j.y) ), item_i.y + item_j.y)
-#            d[i][j] = (dist_x + dist_y) / 10
-
-
-#for i in range(len(d[0])):         #adam
-#    d[i].append(d[i][0])           #adam
+        if i!=j:
+            d[j][i]+=2  
+            d[i][j]+=2 
 
 d.append(d[0])
 for i in range(len(d[0])):
@@ -65,17 +44,6 @@ for i in range(len(d[0])):
     else:
         d[i].append(d[i][0] - 2)
 
-
-#for i in range(len(d)):            #adam
-#    for j in range(len(d) - 1):    #adam
-#       d[i][j] = d[i][j] + 2       #adam
-        
-pprint(np.array(d))
-#nd=len(d)
-#nd0=len(d[0])
-#maxd= max([d[i][j] for i in range(nd) for j in range(nd0)])
-#print(maxd)
-
 from gurobipy import GRB, Model,quicksum
 
 def optimize(part, max_time=90, cart_cap=15, mip_gap=0.0001, print_output=False):
@@ -83,12 +51,8 @@ def optimize(part, max_time=90, cart_cap=15, mip_gap=0.0001, print_output=False)
     m = Model('Supermarket Sweep')
 
     n= len(item_list)
-    # print(n)
     v=[i.price for i in item_list]
     v.append(0.0)
-    # max_time=90
-    # cart_cap=15
-    # mip_gap = 0.0001
 
     x=m.addVars(range(1,n+1), range(2,n+2), vtype=GRB.BINARY, name='x') #if path from item i to j is in the sweep
     y=m.addVars(range(1,n+2), vtype=GRB.CONTINUOUS, name='y') #seconds from start to item i during sweep
@@ -103,22 +67,15 @@ def optimize(part, max_time=90, cart_cap=15, mip_gap=0.0001, print_output=False)
     m.addConstrs(t[i,j]<=max_time*x[i,j] for i in range(1,n+1) for j in range(2,n+2)) # if node i doesn't go to node j, then t[i, j] is 0
     m.addConstrs(y[j]==quicksum([t[i,j] for i in range(1,n+1)]) for j in range(2,n+2)) # y[j] is equal to the only positive t[i, j]
     m.addConstrs(quicksum([t[j,k] for k in range(2,n+2)])==y[j]+quicksum([(d[j-1][k-1])*x[j,k] for k in range(2,n+2) ]) for j in range(1,n+1)) # defines t[i, j] to be the time up to the ith node plus the time from the ith to jth node
-    #m.addConstr(y[n+1]<=max_time) # total time less than 90 seconds
     m.addConstr(quicksum([quicksum([x[i,j] for i in range(1,n+1)]) for j in range(2,n+1)])<= cart_cap) #at most 15 items in the cart (not including the end node)
-    # m.addConstr(quicksum([x[n+1, j] for i in range(1, n+1)]) == 0) # node n+1 must come last
-    # m.addConstrs(y[n+1] >= y[i] for i in range(1, n+1)) #node n+1 must come last
     m.addConstrs(quicksum([x[i,j] for i in range(1,n+1)])== quicksum([x[j,k] for k in range(2,n+2)]) for j in range(2,n+1)) # nodes that arent arrived at are also not departed from
 
-    # m.setObjective(y[n+1],GRB.MINIMIZE) # minimize time to the last node
-    # m.setObjective(quicksum([quicksum([d[i-1][j-1] * x[i, j] for j in range(2, n+2)]) for i in range(1, n+1)]),GRB.MINIMIZE) # same as previous
     m.setObjective(quicksum([v[i-1]*quicksum([x[i,j] for j in range(2,n+2)]) for i in range(1,n+1)]), GRB.MAXIMIZE) # maximize cost of items collected
-
 
     if not print_output:
         m.setParam("LogToConsole", 0)
 
     m.setParam("MIPGap", mip_gap)
-
 
     m.optimize()
 
@@ -130,12 +87,8 @@ def optimize(part, max_time=90, cart_cap=15, mip_gap=0.0001, print_output=False)
                 count +=1
                 nodedict[i]=j
 
-    # pprint(nodedict)
     if print_output:
-        print(f"\n\n\nMoney Won: ${m.ObjVal}")
-        print("")
-
-
+        print(f"\n\n\nMoney Won: ${m.ObjVal}\n")
         print("Path Taken:")
         i = 1
         winnings = 0
@@ -144,7 +97,6 @@ def optimize(part, max_time=90, cart_cap=15, mip_gap=0.0001, print_output=False)
             print(f"Node {i}: {item_list[i-1]}")
             i = nodedict[i]
         print("Back to Start (0,0)\nFinished")
-        # print("winnings " + str(winnings))
 
         time = 0
         for (i, j) in nodedict.items():
@@ -154,11 +106,7 @@ def optimize(part, max_time=90, cart_cap=15, mip_gap=0.0001, print_output=False)
         return m.Runtime
     return m.ObjVal
 
-
-
-
-
-parts = "d"
+parts = "cdef"
 max_times = range(50, 131, 5)
 cart_caps = range(5, 26)
 mip_gaps = [10**-5,10**-4,10**-3,5*10**-3,10**-2,10**-(5/3),10**-(4/3),10**-1,10**-(2/3),10**-(1/3),10**0]
@@ -192,9 +140,7 @@ for part in parts:
     if part == "f":
         f_results = []
         for mip_gap in mip_gaps:
-            op=optimize(mip_gap=(mip_gap), part="f")
-            f_results.append(op)
-            print(op)
+            f_results.append(optimize(mip_gap=(mip_gap), part="f"))
         print("\nF GRAPH")
         plt.plot(mip_gaps,f_results, marker='o')
         plt.xlabel("MIP Parameter in Optimization Problem")
@@ -202,10 +148,3 @@ for part in parts:
         plt.show()
         print("f results")
         print(f_results)
-
-
-#d_results=[120,130,150,180,220]
-#plt.plot([int(i) for i in max_times],d_results, marker='o')
-#plt.xlabel("Number of seconds allotted to competitors (s)")
-#plt.ylabel("Optimal value ($)")
-#plt.show()
